fc35fe73d0390d3431242032e0cac02aecedaeff
Ahmed Mamoun <ahmedbinmamoun@gmail.com>
2025-08-07 15:02:54 +0300

    🌐 Implemented localization with Arabic and English support

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:news/ui/home/home_screen.dart';
 import 'package:news/utils/app_routes.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await Future.delayed(Duration(milliseconds: 1500));
-  runApp(MyApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en'),
+      child: MyApp()
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,11 +24,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      
       initialRoute: AppRoute.homeRouteName,
       routes: {
         AppRoute.homeRouteName : (context) => HomeScreen(),
       },
       themeMode: ThemeMode.dark,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
     );
   }
 } 
